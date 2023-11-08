8d2627571d9879a92baef0676311445f41da6873
Hans Harck Tønning <developer@hansit.dk>
2023-11-08 14:23:50 -0500

    Update tests.ml

--- tests/tests.ml
+++ tests/tests.ml
@@ -4,7 +4,27 @@
 
 open Core
 open OUnit2
+open Lib
 
 let _ = List.find
-let series = "Tests" >::: [ ("" >:: fun _ -> assert_equal (2 + 2) 4) ]
+
+let test_pretty_printer _ =
+  let expected_empty =
+    "-------------------------\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     -------------------------\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     -------------------------\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     |       |       |       |\n\
+     -------------------------\n"
+  in
+  assert_equal expected_empty @@ Sudoku_board.(pretty_print empty)
+
+let series = "Tests" >::: [ "" >:: test_pretty_printer ]
 let () = run_test_tt_main series
