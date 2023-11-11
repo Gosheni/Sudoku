fcccd17eaf097c9894ba77e6398052b51724b8d1
Kushal Dulla <kdulla1@jhu.edu>
2023-11-10 20:53:52 -0500

    added a few more tests

--- tests/tests.ml
+++ tests/tests.ml
@@ -50,8 +50,37 @@ let example_board_ints_1 =
 
 let example_board_1 = create_board example_board_ints_1
 
+let example_board_ints_2 = 
+  [[5;3;4;6;7;8;9;1;2];
+   [6;7;2;1;9;5;3;4;8];
+   [1;9;8;3;4;2;5;6;7];
+   [8;5;9;7;6;1;4;2;3];
+   [4;2;6;8;5;3;7;9;1];
+   [7;1;3;9;2;4;8;5;6];
+   [9;6;1;5;3;7;2;8;4];
+   [2;8;7;4;1;9;6;3;5];
+   [3;4;5;2;8;6;1;7;9]]
+
+let example_board_2 = create_board example_board_ints_2
+
+let example_invalid_board_ints = 
+  [[5;3;4;6;7;8;9;1;2];
+   [6;7;2;1;9;5;3;4;8];
+   [1;9;8;3;4;2;5;6;7];
+   [8;5;9;7;6;1;4;2;3];
+   [4;2;6;8;5;3;7;9;1];
+   [7;1;3;9;4;2;8;5;6];
+   [9;6;1;5;3;7;2;8;4];
+   [2;8;7;4;1;9;6;3;5];
+   [3;4;5;2;8;6;1;7;9]]
+
+let example_invalid = create_board example_invalid_board_ints
+
 let test_is_solved _ =
-  assert_equal true @@ Sudoku_board.is_solved example_board_1
+  assert_equal true @@ Sudoku_board.is_solved example_board_1;
+  assert_equal false @@ Sudoku_board.is_solved Sudoku_board.empty;
+  assert_equal true @@ Sudoku_board.is_solved example_board_2;
+  assert_equal false @@ Sudoku_board.is_solved example_invalid
 
 let series = "Tests" >::: 
 [ "test pretty print" >:: test_pretty_printer;
