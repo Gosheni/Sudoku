d1bbe0e872516e9eec1e8eb9a16f5ea30e8ee4b5
Hans Harck Tønning <developer@hansit.dk>
2023-11-15 14:58:17 -0500

    Update lib.mli

--- src/lib.mli
+++ src/lib.mli
@@ -1,35 +1,29 @@
 module Sudoku_board : sig
-  type element =
-    | Empty
-    | Fixed of int
-    | Volatile of int  (** Contains the board state including which *)
-  [@@deriving yojson]
+  type element = Empty | Fixed of int | Volatile of int
 
-  type row
   type t
 
   val equal : t -> t -> bool
+  (** Compares two sudoku boards, returns true iff the two boards has the same dimension and all the elements in are equal *)
 
   val empty : t
   (** First index is the row *)
 
   val get : t -> int -> int -> element option
-  (** given a board and a position, return Some(element) at that position, 
-                                           None if board is invalid or invalid position given *)
+  (** Given a board and a position, returns the element at that position, 
+      otherwise None is returned if the board is invalid or an invalid position was given *)
   val set : t -> int -> int -> element -> t
-  (** given a board, a position and an element, set the entry on the board at that position to the element *)
+  (** Changes the value at a given coordinate to the given given element. Enforces that the given board is valid. *)
   val set_forced : t -> int -> int -> element -> t
-  (** identical to set except set_forced does not enforce is_valid precondition, used to make invalid test boards *)
+  (** Identical to set except set_forced does not enforce is_valid precondition. Used for creating invalid test boards *)
 
   val is_valid : t -> bool
-  (** checks that the board does not violate any sudoku rules, but could have empty values *)
-
+  (** Checks that the board does not violate any sudoku rules, but could have empty values *)
   val is_solved : t -> bool
-  (** checks that the board is solved i.e. does not violate any sudoku rules and all cells are non-empty *)
+  (** Checks that the board is solved i.e. does not violate any sudoku rules and all cells are non-empty *)
 
   val generate_random : unit -> t
-  (** generate a random fully solved sudoku board *)
-
+  (** Generate a random fully solved sudoku board *)
   val generate_degenerate : t -> int -> t
   (** Takes a fully solved sudoko and removes elements randomly while making sure the resulting board is still solvable. 
      Also takes an int that determines how many elements will be removed. This method expects a fully solved sudoku *)
@@ -54,21 +48,24 @@ module Sudoku_board : sig
   val deserialize : json -> t option
   (** loads a board from json *)
   
-
   val pretty_print : t -> string
   (** prints out the board in a human readable format, for CLI *)
 end
 
 module Sudoku_game : sig
-  (** Fixed cell is used when the user attempts to change a cell that is fixed. Already present is used when the user's move would make a row/column/3x3 square have a duplicate entry *)
-  type error_states = Fixed_cell | Already_present | Invalid_position
+  (** Interface for the game *)
+  type error_states =
+    | Fixed_cell
+    | Already_present
+    | Invalid_position
+        (** Fixed cell is used when the user attempts to change a cell that is fixed. Already present is used when the user's move would make a row/column/3x3 square have a duplicate entry *)
 
   type move = { x : int; y : int; value : int option }
 
   type hint =
     | Incorrect_cell of (int * int)
     | Suggested_move of move
-    | Already_solved
+    | Already_solved  (** The kinds of hints allowed *)
 
   val do_move : Sudoku_board.t -> move -> (Sudoku_board.t, error_states) result
   (** Fails if attempting to change a fixed cell or the user makes a blatantly invalid move, like adding a 2 to a row that already contains a 2. If the move succeeds the updated board will be returned *)
