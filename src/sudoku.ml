880ca1298e9c85073c0a5dde171d0fe61856e0b9
Gosheni <euihyunc31@gmail.com>
2023-11-28 15:39:23 -0500

    Improved code style

--- src/sudoku.ml
+++ src/sudoku.ml
@@ -9,47 +9,35 @@ let () =
         anon (maybe (sequence ("arg" %: string)))
       in
     fun () ->
-      match String.lowercase command_string with
-      | "init" | "hint" | "solve" ->
-        (match command_args with
-        | None ->
-          (match command_string with
-          | "init" -> Stdio.print_endline "Initialized a new game!"
-          | "hint" -> Stdio.print_endline "Possible move is 8 at 2, 9"
-          | "solve" -> Stdio.print_endline "Solved the Sudoku game!"
-          | _ -> ());
-        | _ -> Stdio.print_endline "Invalid arguments for command";
-        );
-      | "move" ->
-        (match command_args with
-        | Some args ->
-          (match args with
-           | [a; b; c] ->
-             (try
-                let value = int_of_string a in
-                let row = int_of_string b in
-                let col = int_of_string c in
-                if (1 <= value && value <= 9) && (1 <= row && row <= 9) && (1 <= col && col <= 9) then
-                  Stdio.printf "Made a move: Add value %d to row %d col %d\n" value row col
-                else
-                  Stdio.print_endline "Invalid arguments for move command: Values out of range (1-9)"
-              with
-              | Failure _ -> Stdio.print_endline "Invalid arguments for move command")
-           | _ -> Stdio.print_endline "Invalid arguments for move command";
-           );
-        | None -> Stdio.print_endline "No arguments provided for move command";
-        );
-      | "save" ->
-        (match command_args with
-        | Some args ->
-          if List.length args = 1 && List.for_all args ~f:(fun arg -> String.is_suffix arg ~suffix:".json") then
-            Stdio.printf "Current board saved with args: %s\n" (List.hd_exn args)
+      match (String.lowercase command_string, command_args) with
+      | ("init", None) ->
+        Stdio.print_endline "Initialized a new game!"
+      | ("hint", None) ->
+        Stdio.print_endline "Possible move is 8 at 2, 9"
+      | ("solve", None) ->
+        Stdio.print_endline "Solved the Sudoku game!"
+      | ("move" | "save"), None ->
+        Stdio.print_endline "No arguments provided for move or save command"
+      | ("init" | "hint" | "solve"), Some _ ->
+        Stdio.print_endline "Invalid arguments for command"
+      | "move", Some [a; b; c] ->
+        (try
+          let value = int_of_string a in
+          let row = int_of_string b in
+          let col = int_of_string c in
+          if (1 <= value && value <= 9) && (1 <= row && row <= 9) && (1 <= col && col <= 9) then
+            Stdio.printf "Made a move: Add value %d to row %d col %d\n" value row col
           else
-            Stdio.print_endline "Invalid arguments for save command. Expected a single JSON file.";
-        | None ->
-          Stdio.print_endline "No arguments provided for save command";
-        );
+            Stdio.print_endline "Invalid arguments for move command: Values out of range (1-9)"
+        with
+        | Failure _ -> Stdio.print_endline "Invalid arguments for move command")
+      | "move", Some _ ->
+        Stdio.print_endline "Invalid arguments for move command"
+      | "save", Some [arg] when String.is_suffix arg ~suffix:".json" ->
+        Stdio.printf "Current board saved with args: %s\n" arg
+      | "save", Some _ ->
+        Stdio.print_endline "Invalid filename"   
       | _ ->
-        Stdio.print_endline "Invalid command";
+        Stdio.print_endline "Invalid command"   
     ) 
   |> Command_unix.run
