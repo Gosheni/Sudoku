f470644a954a84053b1a4547c5b0093ea391bc8f
Hans Harck Tønning <developer@hansit.dk>
2023-12-18 20:53:32 -0500

    Updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
-rm ./*.coverage;
-rm ./*.json;
-rm ./sudoku.config
+dune clean;
+rm ./*.coverage 2> /dev/null;
+rm -r ./_coverage 2> /dev/null;
+rm ./*.json 2> /dev/null;
+rm ./sudoku.config 2> /dev/null
